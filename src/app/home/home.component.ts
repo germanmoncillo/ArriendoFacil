906888b15922d4e1f606eb2606b663c0fdcf22cd
jaime-yamid <139189571+jaime-yamid@users.noreply.github.com>
2024-03-29 17:11:10 -0500

    intentando de integrar un inicio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SlickCarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  images = [
+    '../../assets/imag1.jpg',
+    '../../assets/imag2.jpg',
+    '../../assets/imag3.jpg'
+  ];
 }
+
+
